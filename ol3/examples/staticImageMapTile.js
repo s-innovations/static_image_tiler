e5fecdd5376f81d54a8c675b2c8beb3197067bb5
Poul Kjeldager Sørensen <pks@s-innovations.net>
2014-09-17 17:35:41 +0200

    added example for map render

---
+++ ol3/examples/staticImageMapTile.js
@@ -0,0 +1,32 @@
+// Maps always need a projection, but the static image is not geo-referenced,
+// and are only measured in pixels.  So, we create a fake projection that the
+// map can use to properly display the layer.
+var pixelProjection = new ol.proj.Projection({
+  code: 'EPSG:3857', 
+});
+
+var map = new ol.Map({
+  layers: [
+    new ol.layer.Image({
+      source: new ol.source.ImageTilesStatic({
+        attributions: [
+          new ol.Attribution({
+            html: '&copy; <a href="http://www.s-innovations.net/">xkcd</a>'
+          })
+        ],
+        url: 'https://raw.githubusercontent.com/s-innovations/static_image_tiler/master/data/out/{z}/{x}/{y}.jpg',
+        imageSize: [7360,4912],
+        perspectiveWarp: [[0,0,0],[0,0,0],[0,0,0]],
+        imageResolution:0.04,
+        projection: pixelProjection,
+        imageExtent: pixelProjection.getExtent()
+      })
+    })
+  ],
+  target: 'map',
+  view: new ol.View({
+    projection: pixelProjection,
+    center: ol.extent.getCenter(pixelProjection.getExtent()),
+    zoom: 2
+  })
+});
